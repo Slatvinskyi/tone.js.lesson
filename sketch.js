39f6e5e387f1c3ae413df71b7a1bf563329171f1
Slatvinskyi <andrii.slatvinskyi@gmail.com>
2021-09-06 21:24:20 +0300

    finished

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
 let ready = false;
 
-let osc; 
+let osc;
+let osc2; 
+let lfo;
 
 let wave;
 
@@ -10,12 +12,20 @@ let wave;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  
+
   osc = new Tone.Oscillator();
+  //osc.type = 'triangle'; 4 differents types: sine (default), square, triangle and sawtooth
 
-  osc.frequency.value = 220;// 220 hrz / A3
-
-  //osc.connect(Tone.Master);
+  //osc.frequency.value = 220;// 220 hrz / A3 //osc.connect(Tone.Master);
   osc.toDestination();// or like this) this is the thing which is outputting the audio
+
+  osc2 = new Tone.Oscillator();
+  osc2.frequency.value = 220;// 220 hrz / A3 //osc.connect(Tone.Master);
+  osc2.toDestination();// or like this) this is the thing which is outputting the audio
+
+  lfo = new Tone.LFO("0.1hz", 210, 230);
+  //lfo.connect(osc.frequency);
 
   wave = new Tone.Waveform();
   Tone.Master.connect(wave);
@@ -39,11 +49,21 @@ function draw() {
     stroke(255);
 //visualisation
     let buffer = wave.getValue(0);
-    for (let i=1; i < buffer.length; i++){ //i=0 for dots
-        let x1 = map(i-1, 0, buffer.length, 0, width);
+    //from negative to positive
+    let start = 0;
+    for (let i=1; i < buffer.length; i++){
+        if (buffer[i-1] < 0 && buffer[i] >= 0) {
+            start = i; 
+            break; //interrupt the loop
+        }
+    }
+//drawing the wave
+    let end = start + buffer.length/2;
+    for (let i=start; i < end; i++){ //i=0 for dots
+        let x1 = map(i-1, start, end, 0, width);
         let y1 = map(buffer[i-1], -1, 1, 0, height);
 
-        let x2 = map(i, 0, buffer.length, 0, width);
+        let x2 = map(i, start, end, 0, width);
         let y2 = map(buffer[i], -1, 1, 0, height);
         
         line(x1,y1,x2, y2);
@@ -54,7 +74,7 @@ function draw() {
     fill(255);
     noStroke();
     textAlign(CENTER, CENTER);
-    text(" START", width / 2, height / 2);
+    text("START", width / 2, height / 2);
   }
 }
 
@@ -63,6 +83,8 @@ function mousePressed() {
 
 
     osc.start();
+    osc2.start();
+    //lfo.start();
     ready = true;
   }
   
